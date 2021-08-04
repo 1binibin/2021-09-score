52e15b24e2dfbb0ab8e0b1c1d2f274409837c3a8
1binibin <namwonbin@gmail.com>
2021-08-04 17:23:56 +0900

    성적관리시스템 완성

--- public/js/index.js
+++ public/js/index.js
@@ -20,6 +20,17 @@ var user = null;
 
 
 /*************** event callback ********************/
+function onAdded(v) {
+    var n = $('.test-wrapper .tbody tr').length + 1;
+    var html = '<tr>';
+    html += '<td>'+n+'</td>';
+    html += '<td>'+v.val().username+'</td>';
+    html += '<td class="text-left">'+v.val().comment+'</td>';
+    html += '<td>'+moment(v.val().createdAt).format('YYYY-MM-DD HH:mm:ss')+'</td>';
+    html += '</tr>';
+    $(html).prependTo('.test-wrapper .tbody');
+}
+
 function onSubmit(f) {
     if(!user) alert('로그인 후 사용해 주세요.');
     else{
@@ -34,6 +45,7 @@ function onSubmit(f) {
         db.ref('root/test').push(data);
         f.username.value = '';
         f.reset();
+        f
         }
     }
     return false;
@@ -42,6 +54,7 @@ function onSubmit(f) {
 function onAuthChanged(v) {// auth 상태가 변하면 알려줘!
     user = v;
     if(user) {
+        db.ref('root/test').on('child_added', onAdded);
         $('.bt-login').hide();
         $('.bt-logout').show();
         $('.photo-logo img').attr('src', user.photoURL);
@@ -54,6 +67,7 @@ function onAuthChanged(v) {// auth 상태가 변하면 알려줘!
         $('.photo-logo').hide();
         $('.icon-logo').show();
     }
+    $('.test-wrapper .tbodfy').empty();
 }
 
 function onLogin() {
