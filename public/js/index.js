e4a32dbb39ca3381273a46a29c65ecf1cb58d332
1binibin <namwonbin@gmail.com>
2021-08-04 15:07:38 +0900

    5교시 끝

--- public/js/index.js
+++ public/js/index.js
@@ -1,27 +1,52 @@
+/* 
+false 판정 :0, null, undefined, '', false
+true 판정 : false판정 외 모든것
+- 0을 제외한 모든 숫자
+- ''(빈문자열)을 제외한 모든 문자.
+-객체, 함수, ...전부다
+*/
 /*************** Global init ********************/
 //console.log(firebase);
 var auth = firebase.auth();
 var db = firebase.database();
 var googleAuth = new firebase.auth.GoogleAuthProvider();
 
+// console.log(auth, db, googleAuth);
 
 
 /*************** function init ********************/
 
 
 /*************** event callback ********************/
-function onAuthChanged() {
-
+function onAuthChanged(user) {// auth 상태가 변하면 알려줘!
+    if(user) {
+        console.log(user);
+        $('.bt-login').hide();
+        $('.bt-logout').show();
+        $('.photo-logo img').attr('src', user.photoURL);
+        $('.photo-logo').show();
+        $('.icon-logo').hide();
+    }
+    else {
+        $('.bt-login').show();
+        $('.bt-logout').hide();
+        $('.photo-logo').hide();
+        $('.icon-logo').show();
+    }
 }
 
 function onLogin() {
     auth.signInWithPopup(googleAuth);
 }
 
+function onLogout() {
+    auth.signOut();
+}
 
 /*************** event init ********************/
-auth.onAuthStateChanged(onAuthChanged);
+auth.onAuthStateChanged(onAuthChanged); // auth 상태가 변하면 알려줘!
 $('.bt-login').click(onLogin);
+$('.bt-logout').click(onLogout);
 
 
 
